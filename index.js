558af81d0f74f92abd5b90010a406102755bb0ae
Mhd Fadhil Arya Putra <fadhilgaming522@gmail.com>
2023-02-25 19:36:20 +0300

    first commit

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
